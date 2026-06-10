a2c477f26ac3fbf89f79e1bc73d5ec68c9ae0e49
PedroNapp <pedro.napp@rede.ulbra.br>
2026-06-10 16:21:17 -0300

    Add files via upload
    Test pra verificar se a primeira workFlow funciona

---
+++ app.js
@@ -0,0 +1,48 @@
+function calcularSubtraçao(){
+    let n1 = parseFloat(document.getElementById("numero1").value);
+    let n2 = parseFloat(document.getElementById("numero2").value);
+
+    let resultado = n1 - n2;
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function calcularSoma(){
+
+    let n1 = parseFloat(document.getElementById("numero1").value);
+    let n2 = parseFloat(document.getElementById("numero2").value);
+
+    let resultado = n1 + n2;
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function calcularMultiplicacao(){
+
+    let n1 = parseFloat(document.getElementById("numero1").value);
+    let n2 = parseFloat(document.getElementById("numero2").value);
+
+    let resultado = n1 * n2;
+    document.getElementById('resultado').innerHTML = resultado;
+    
+}
+
+function calcularDivisao(){
+    let n1 = parseFloat(document.getElementById("numero1").value);
+    let n2 = parseFloat(document.getElementById("numero2").value);
+
+    let resultado = n1 / n2;
+
+    if (n2 == 0) {
+        alert('Impossível divisão por 0')
+        return;
+    }
+    
+    document.getElementById('resultado').innerHTML = resultado;
+    
+}
+function limparResultado(){
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+    document.getElementById('resultado').innerHTML = '';
+}
